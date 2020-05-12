19717e07f3fa716969cf9e8e8531f7285f703d7e
Aditya Subramanian <subramanianaditya42@gmail.com>
2020-05-12 18:25:21 +0200

    Add raw data and curated standings data

---
+++ data/standingsData.js
@@ -0,0 +1,47 @@
+const standingsData = {
+  ALW: [
+    //Team name, Wins, Losses, GB
+    ["Houston Astros", 107, 55, null],
+    ["Oakland Athletics", 97, 65, 10],
+    ["Texas Rangers", 78, 84, 29],
+    ["Los Angeles Angels", 72, 90, 35],
+    ["Seattle Mariners", 68, 94, 39],
+  ],
+  ALE: [
+    ["New York Yankees", 103, 59, null],
+    ["Tampa Bay Rays", 96, 66, 7],
+    ["Boston Red Sox", 84, 78, 19],
+    ["Toronto Blue Jays", 87, 95, 36],
+    ["Baltimore Orioles", 54, 108, 49],
+  ],
+  ALC: [
+    ["Minnesota Twins", 101, 61, null],
+    ["Cleveland Indians", 93, 69, 8],
+    ["Chicago White Sox", 72, 89, 28.5],
+    ["Kansas City Royals", 59, 103, 42],
+    ["Detroit Tigers", 47, 114, 53.5],
+  ],
+  NLW: [
+    ["Los Angeles Dodgers", 106, 56, null],
+    ["Arizona Diamondbacks", 85, 77, 21],
+    ["San Francisco Giants", 77, 85, 29],
+    ["Colorado Rockies", 71, 91, 35],
+    ["San Diego Padres", 70, 92, 36],
+  ],
+  NLC: [
+    ["St.Louis Cardinals", 91, 71, null],
+    ["Milwaukee Brewers", 89, 73, 2],
+    ["Chicago Cubs", 84, 78, 7],
+    ["Cincinnati Reds", 75, 87, 16],
+    ["Pittsburgh Pirates", 69, 93, 22],
+  ],
+  NLE: [
+    ["Atlanta Braves", 97, 65, null],
+    ["Washington Nationals", 93, 69, 4],
+    ["New York Mets", 86, 76, 11],
+    ["Philadelphia Phillies", 81, 81, 16],
+    ["Miami Marlins", 57, 105, 40],
+  ],
+};
+
+export default standingsData;
