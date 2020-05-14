20bafa77ec37bbecb743b512204fc302ab4897fe
Aditya Subramanian <subramanianaditya42@gmail.com>
2020-05-14 15:36:12 +0200

    Add basic styling to /index

--- src/components/DivsionStandings.js
+++ src/components/DivsionStandings.js
@@ -1,21 +1,33 @@
 import React from "react";
 import standingsData from "../data/standingsData";
 import Division from "./Division";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  item: {
+    width: "33%",
+  },
+});
 function DivisionStandings() {
+  const classes = useStyles();
   const divsionNames = []; //TODO: Refactor this to be more elegant at some point please :)
   for (const divisionAbbreviations in standingsData) {
     divsionNames.push(divisionAbbreviations);
   }
+
   return (
-    <div>
+    <Grid container direction="row" justify="space-between">
       {divsionNames.map((divisionName, index) => (
-        <Division
-          data={standingsData[divisionName]}
-          divisionName={divisionName}
-          key={index} //TODO: Add unique ID
-        />
+        <Grid item className={classes.item}>
+          <Division
+            data={standingsData[divisionName]}
+            divisionName={divisionName}
+            key={index} //TODO: Add unique ID
+          />
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 }
 
--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -3,11 +3,20 @@ import { SearchContext } from "./../context/SearchContext";
 import API from "../lib/api-services";
 import standingsData from "../data/standingsData";
 import DivisionStandings from "./DivsionStandings";
+import { makeStyles } from "@material-ui/core";
+import "./../styles/index.css";
 
+const useStyles = makeStyles({
+  container: {
+    width: "5 0%",
+    border: "red solid 1px",
+  },
+});
 function IndexPage() {
+  const classes = useStyles();
   return (
-    <div>
-      <DivisionStandings />
+    <div className={classes.container}>
+      <DivisionStandings className="container" />
     </div>
   );
 }
